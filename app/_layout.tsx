f32d8f9e089dd4ab67b07a221a23ebba2f9a9b74
Chanachai04 <chn.benmat@gmail.com>
2026-04-18 18:42:42 +0700

    แก้ layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,60 +1,8 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Stack } from "expo-router";
-import { useEffect, useState } from "react";
-import { ActivityIndicator, View } from "react-native";
-import { Theme } from "../constants/theme";
 
 export default function RootLayout() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [initialRoute, setInitialRoute] = useState<"(auth)" | "(tabs)">(
-    "(auth)",
-  );
-
-  useEffect(() => {
-    bootstrapAsync();
-  }, []);
-
-  const bootstrapAsync = async () => {
-    try {
-      const sessionStr = await AsyncStorage.getItem("user_session");
-      if (sessionStr) {
-        const session = JSON.parse(sessionStr);
-        if (session?.user?.id) {
-          setInitialRoute("(tabs)");
-        } else {
-          setInitialRoute("(auth)");
-        }
-      } else {
-        setInitialRoute("(auth)");
-      }
-    } catch (error) {
-      console.error("Error checking session:", error);
-      setInitialRoute("(auth)");
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  if (isLoading) {
-    return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: Theme.colors.surface,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <ActivityIndicator size="large" color={Theme.colors.primary} />
-      </View>
-    );
-  }
-
   return (
-    <Stack
-      screenOptions={{ headerShown: false }}
-      initialRouteName={initialRoute}
-    >
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
